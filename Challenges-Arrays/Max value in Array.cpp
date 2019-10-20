665f1d148699655a9c3867e549bf473444b2fee6
Abhinavg247 <54761789+Abhinavg247@users.noreply.github.com>
2019-10-21 00:23:42 +0530

    Problem 1 from Arrays

---
+++ Challenges-Arrays/Max value in Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	long int a[100];
+	int n;
+	cin>>n;
+	int max=INT_MIN;
+
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+		if(a[i]>=(-1000000000)&& a[i]<=1000000000)
+		{
+			if(a[i]>max)
+			{
+				max=a[i];
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	cout<<max<<endl;
+	return 0;
+}
